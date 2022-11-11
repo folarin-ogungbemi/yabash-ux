a436da80b1fe559b60e95d15e4bb24f2a294f413
Folarin Ogungbemi <Folarine1@gmail.com>
2022-11-11 21:01:31 +0000

    Add nav to signup page

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -2,12 +2,12 @@
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 3,
-      // Berlin Longitude and Latitude
+      // Berlin Latitude and Longitude
       center: { lat: 52.501408, lng: 13.4023285 },
     });
     // Create an array of alphabetical characters used to label the markers.
   const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  // Alexanderplatz Longitude and Latitude
+  // Alexanderplatz Latitude and Longitude
   const locations = [{lat: 52.521992, lng:13.413244}]
   // Add some markers to the map.
   const markers = locations.map((location, i) => {
