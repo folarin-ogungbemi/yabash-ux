0a585fe64b0a02606158cc1f666a165c2479b119
Folarin Ogungbemi <Folarine1@gmail.com>
2022-11-13 12:41:37 +0000

    Add alert for booking

--- assets/js/bookingEmail.js
+++ assets/js/bookingEmail.js
@@ -1,17 +1,25 @@
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     const LC = document.write("<br>");
-// })
+function reserve(event) {
+    var bookEvent = document.getElementById('selectEvent').value;
+    var date = document.getElementById('selectDate').value;
+    var time = document.getElementById('selectTime').value;
+    var guest = document.getElementById('selectPeople').value;
+    var confirmationText = `You have made the following reservation: \n\n - Booked Event: ${ bookEvent}\n - Date: ${ date} \n - Time: ${ time}\n - Number of Guest: ${ guest} \n\n Confirm Reservation?`;
+    var valid = confirm(confirmationText);
+    if (valid == false){
+        event.preventDefault();
+    }
+}
 
 function sendMail(contactForm) {
     emailjs.send("service_4frrkbj", "yabash GmbH", {
         "from_name": 'Customer',
         "from_email": 'Customer@gmail.com',
-        "message":  "Date : " + contactForm.selectDate.value + "*************************"
-            +" Time : " + contactForm.selectTime.value + "*************************"
-            +" Number of Guest : " + contactForm.selectPeople.value + "*************************"
-            +" Type of Event : " + contactForm.selectEvent.value + "*************************"
-            +" Additional Information : " + contactForm.eventInfo.value
+        "message":  `Date : ${contactForm.selectDate.value}
+            Time : ${contactForm.selectTime.value}
+            Number of Guest : ${contactForm.selectPeople.value}
+            Type of Event : ${contactForm.selectEvent.value}
+            Additional Information : ${contactForm.eventInfo.value}`
     })
     .then(
         function(response) {
